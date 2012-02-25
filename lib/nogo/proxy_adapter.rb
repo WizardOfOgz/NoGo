7cfcbbc67ea2a57e2d2f479d5855b3ef34209ed4
Andy Ogzewalla <andy@socialvolt.com>
2012-02-25 02:41:41 -0600

    set and get strategy

--- lib/nogo/proxy_adapter.rb
+++ lib/nogo/proxy_adapter.rb
@@ -1,7 +1,9 @@
 module NoGo
   class ProxyAdapter
-    instance_methods.each do |m| 
-      undef_method m unless m =~ /^__|^send$|^object_id$|^extend|^tap/ 
+    StrategyOptions = [:raise, :warn, :pass_through]
+
+    instance_methods.each do |method_name| 
+      undef_method method_name unless method_name =~ /^__|^send$|^object_id$|^extend|^tap|^instance_variable_set|^instance_variable_get/ 
     end
 
     def initialize(adapter)
@@ -10,6 +12,19 @@ module NoGo
       ) unless adapter.is_a?(ActiveRecord::ConnectionAdapters::AbstractAdapter)
 
       @adapter = adapter
+      @strategy = :raise
+    end
+
+    def strategy
+      @strategy
+    end
+
+    def strategy=(strategy_option)
+      raise ArgumentError.new(
+        "Expected strategy to be set to one of [:raise, :warn, :pass_through], but received #{strategy_option}"
+      ) unless StrategyOptions.include?(strategy_option.to_sym)
+
+      @strategy = strategy_option
     end
 
     private
@@ -19,3 +34,72 @@ module NoGo
     end
   end
 end
+
+  # def execute_with_db_safe(sql, name = nil)
+  #   return if sql.match /BEGIN|ROLLBACK|SAVEPOINT/
+  #   puts "---> EXECUTE: #{sql}"
+  #   raise_or_noop
+  # end
+  
+  # def insert_with_db_safe(sql, name = nil, pk = nil, id_value = nil, sequence_name = nil, binds = [])
+  #   puts "---> INSERT: #{sql.to_sql}"
+  #   raise_or_noop
+  # end
+
+  # def select_rows_with_db_safe(sql, name = nil)
+  #   raise_or_noop []
+  # end
+
+  # protected
+  
+  # def raise_or_noop(noop_return_value = nil)
+  #   DBSafe.enabled ? raise(EXCEPTION_MESSAGE) : noop_return_value
+  # end
+  
+  # def select_with_db_safe(sql, name = nil, binds = [])
+  #   raise_or_noop []
+  # end
+
+
+
+  # def execute(sql, name = nil)
+  #   raise_or_noop
+  # end
+  
+  # def insert(sql, name = nil, pk = nil, id_value = nil, sequence_name = nil)
+  #   raise_or_noop
+  # end if Rails::VERSION::MAJOR == 1
+
+  # def select_rows(sql, name = nil)
+  #   raise_or_noop []
+  # end
+  
+  # def rename_table(table_name, new_name)
+  #   raise_or_noop
+  # end
+  
+  # def change_column(table_name, column_name, type, options = {})
+  #   raise_or_noop
+  # end
+  
+  # def change_column_default(table_name, column_name, default)
+  #   raise_or_noop
+  # end
+
+  # def rename_column(table_name, column_name, new_column_name)
+  #   raise_or_noop
+  # end
+  
+  # def tables
+  #   @cached_columns.keys
+  # end
+
+  # protected
+  
+  # def raise_or_noop(noop_return_value = nil)
+  #   @strategy == :raise ? raise(EXCEPTION_MESSAGE) : noop_return_value
+  # end
+  
+  # def select(sql, name = nil)
+  #   raise_or_noop []
+  # end
--- spec/proxy_adapter_spec.rb
+++ spec/proxy_adapter_spec.rb
@@ -16,10 +16,47 @@ describe NoGo::ProxyAdapter do
     it 'raises error when adapter argument is not an instance of AbstractAdapter' do
       expect{ NoGo::ProxyAdapter.new(Object.new) }.to raise_error(ArgumentError)
     end
+    
+    it 'initializes @strategy to :raise' do
+      NoGo::ProxyAdapter.new(adapter).instance_variable_get(:@strategy).should == :raise
+    end
   end
 
   it 'passes method calls through to the adapter' do
     adapter.should_receive(:pass_through)
     proxy_adapter.pass_through
+  end
+
+  describe '#strategy' do
+    NoGo::ProxyAdapter::StrategyOptions.each do |strategy_option|
+      it "returns :#{strategy_option}" do
+        proxy_adapter.instance_variable_set(:@strategy, strategy_option)
+        proxy_adapter.strategy.should == strategy_option
+      end
+    end
+  end
+
+  describe '#strategy=' do
+    NoGo::ProxyAdapter::StrategyOptions.each do |strategy_option|
+      it "sets :#{strategy_option} strategy" do
+        proxy_adapter.strategy = strategy_option
+        proxy_adapter.strategy.should == strategy_option
+      end
+    end
+
+    it 'raises an exception if the strategy option is not valid' do
+      expect{ proxy_adapter.strategy = :invalid_option }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#raise_or_pass_through' do
+    context 'strategy is :raise' do
+      before :each do
+        # proxy_adapter.strategy = :raise
+      end
+
+      it 'raises an exception in ' do
+      end
+    end
   end
 end
